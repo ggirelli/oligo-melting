2a15aabd340f31231a9aebe0b0177b2bcd39af7f
Gabriele Girelli <gigi.ga90@gmail.com>
2019-03-15 15:45:41 +0100

    Minimized

--- oligo_melting/__init__.py
+++ oligo_melting/__init__.py
@@ -1,19 +1,1 @@
-# -*- coding: utf-8 -*-
-
-'''
-@author: Gabriele Girelli
-@email: gigi.ga90@gmail.com
-@description: oligo_melting package.
-'''
-
-# DEPENDENCIES =================================================================
-
-__all__ = []
-
-import math
-
 from oligo_melting.melt import *
-
-# END ==========================================================================
-
-################################################################################
